8f0a3b75d1f73e0faface1ab5ff1fbf83d6fc70c
徐小坤 <996670910@qq.com>
2019-03-07 09:46:30 +0800

    添加 button

---
+++ src/button.vue
@@ -0,0 +1,30 @@
+<template>
+    <button class="g-button">按钮</button>
+</template>
+
+<script>
+    export default {
+
+    }
+</script>
+
+<style lang="scss">
+    .g-button{
+        font-size: var(--font-size);
+        height: var(--button-height);
+        color: var(--color);
+        padding: 0 1em;
+        background: var(--button-bg);
+        border: 1px solid var(--border-colr);
+        border-radius: var(--border-radius);
+        outline: none;
+        cursor: pointer;
+        &:hover{
+            border-color: var(--border-color-hover);
+        }
+        &:active{
+            background-color: var(--button-active-bg);
+        }
+    }
+
+</style>
